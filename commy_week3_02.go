10eb10a576a626956752dfaf74b7eba8519712b9
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-11-30 17:37:12 +0700

    fmt.Println

--- commy_week3_02.go
+++ commy_week3_02.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func main() {
 	n, e := fmt.Println("kamonrachan", "tokoksung", 013, 123)
-
+	fmt.Println("number of bytes written :", n)
+	fmt.Println("write error encounterd :", e,)
 }
