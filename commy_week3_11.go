4ded6426f92ce5a0ba47ab40d0ae0fd5bc4f01ad
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-12-01 13:50:27 +0700

    ภายใต้ fmt มีฟังก์ชันชื่อ Println

--- commy_week3_11.go
+++ commy_week3_11.go
@@ -8,5 +8,8 @@ func main() {
 	var age int
 	var height float32
 	var weigth float32
-	
+	n, err := fmt.Scan(&name, &age, &height, &weigth)
+	fmt.Println(name, age, height, weigth)
+	fmt.Println(`number of argument`, n)
+	fmt.Println(`error `, err)
 }
