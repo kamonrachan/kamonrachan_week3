288bd75c8da625046338f33c1f1e469db7348155
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-12-01 13:19:17 +0700

    ภายใต้ fmt มีฟังก์ชันชื่อ Printf ให้เราสามารถพิมพ์ข้อความออกจอได้

--- commy_week3_09.go
+++ commy_week3_09.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	
+	fmt.Printf("%t\n", 1>1)
+	fmt.Printf("%t\n", 1>2)
+	fmt.Printf("%t\n", 5<1)
+	fmt.Printf("%t\n", 1<5)
+	fmt.Printf("%t\n", 110>100)
+	fmt.Printf("%t\n", 1521>=20)
+
 }
