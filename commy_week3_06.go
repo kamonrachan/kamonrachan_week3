5be67c3675a808b4f091701f122338b56ff79611
kamonrachan <kamonrachan.to@rmuti.ac.th>
2019-12-01 12:15:17 +0700

    ..

--- commy_week3_06.go
+++ commy_week3_06.go
@@ -5,9 +5,9 @@ import "fmt"
 func main() {
 	var name string
 	var age int
-n, e := fmt.Scanln(&name, &age)
-fmt.Println("name", name)
-fmt.Println("age", age)
-fmt.Println("number of items successfully scanned ", n)
-fmt.Println("error", e)
+	n, e := fmt.Scanln(&name, &age)
+	fmt.Println("name", name)
+	fmt.Println("age", age)
+	fmt.Println("number of items successfully scanned ", n)
+	fmt.Println("error", e)
 }
